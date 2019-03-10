75e0eeb3549db09502bf5b5f00e2cf9edf832fe7
panja <panja@DESKTOP-KTIE4PK.lan1>
2019-03-10 14:28:09 -0700

    String and Arrays

--- src/algorithms/LinearSearch.java
+++ src/algorithms/LinearSearch.java
@@ -1,33 +1,34 @@
 package algorithms;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-
 public class LinearSearch {
 
+	/*
+	 * public static void main(String[] args) { List<Integer> no =
+	 * Arrays.asList(1,2,3,9); List<Integer> list = Arrays.asList(1,2,4,4,9);
+	 * Integer sum = 8; System.out.println(hasPairWithSum(no,sum));
+	 * System.out.println(hasPairWithSum(list,sum)); }
+	 * 
+	 * private static boolean hasPairWithSum(List<Integer> list, Integer sum) {
+	 * 
+	 * Map<Integer,Integer> comp = new HashMap<Integer,Integer>();
+	 * //HashSet<Integer> s= new HashSet<Integer>(); for(int value:list) {
+	 * 
+	 * if(!comp.isEmpty() && comp.get(value)!=comp.get(comp.size()-1)) return true;
+	 * comp.put(sum-value, sum-value); } return false; }
+	 */
 	public static void main(String[] args) {
-		List<Integer> no = Arrays.asList(1,2,3,9);
-		List<Integer> list = Arrays.asList(1,2,4,9);
-		Integer sum = 8;
-		System.out.println(hasPairWithSum(no,sum));
-		System.out.println(hasPairWithSum(list,sum));
+		int arr[] = {2,9,5,7,23,17,43};
+		int x=17;
+		System.out.println("the position for given number is:" +findNumber(arr,x));
 	}
 
-	private static boolean hasPairWithSum(List<Integer> list, Integer sum) {
-		
-		Map<Integer,Integer> comp = new HashMap<Integer,Integer>();
-		HashSet<Integer> s= new HashSet<Integer>();
-		for(int value:list)
-		{
-			
-			if(!comp.isEmpty() && comp.get(value)!=comp.get(comp.size()-1))
-				return true;
-			comp.put(sum-value, sum-value);
-		}
-		return false;
+	private static int findNumber(int[] arr, int x) {
+for(int i=0; i<=arr.length; i++)
+{
+	if(arr[i]==x)
+		return i;
+}
+return -1;
 	}
 
 }
--- src/algorithms/MergeSort.java
+++ src/algorithms/MergeSort.java
@@ -2,22 +2,38 @@ package algorithms;
 
 public class MergeSort {
 
-	private void merge(int[] arr, int first, int middle, int last) {
-		int fn = middle - first + 1;
-		int ln = last - middle;
+	public static void main(String[] args) {
+		int arr[] = { 23, 12, 5, 6, 32, 45, 11 };
+		mergeSort(arr, 0, arr.length - 1);
+		pirntArray(arr);
+	}
 
-		int[] ftemp = new int[fn];
-		int[] ltemp = new int[ln];
+	private static void mergeSort(int[] arr, int first, int last) {
+		if (first < last) {
+			int mid = (first + last) / 2;
+			mergeSort(arr, first, mid);
+			mergeSort(arr, mid+1, last);
+  			merge(arr, first, mid, last);
+		}
+	}
 
-		for (int i = 0; i < fn; ++i)
+	private static void merge(int[] arr, int first, int mid, int last) {
+		//23, 12, 5, 6, 32, 45, 11 
+		int fnum = mid-first + 1;
+		int lnum = last - mid;
+		
+		int ftemp[] = new int[fnum];
+		int ltemp[] = new int[lnum];
+		
+		for (int i = 0; i < fnum; i++)
 			ftemp[i] = arr[first + i];
 		
-		for (int j = 0; j < ln; ++j)
-			ltemp[j] = arr[middle + 1 + j];
+		for (int j = 0; j < lnum; j++)
+			ltemp[j] = arr[mid + 1 + j];
 		
-		int i = 0, j = 0;
-		int k = first;
-		while (i < fn && j < ln) {
+		int i = 0, j = 0, k = first;
+		
+		while (i < fnum && j < lnum) {
 			if (ftemp[i] <= ltemp[j]) {
 				arr[k] = ftemp[i];
 				i++;
@@ -28,14 +44,13 @@ public class MergeSort {
 			k++;
 		}
 
-		while (i < fn) {
+		while (i < fnum) {
 			arr[k] = ftemp[i];
 			i++;
 			k++;
-
 		}
 
-		while (j < ln) {
+		while (j < lnum) {
 			arr[k] = ltemp[j];
 			j++;
 			k++;
@@ -43,32 +58,49 @@ public class MergeSort {
 
 	}
 
-	void mergeSort(int[] arr, int first, int last) {
-		if (first < last) {
-			int middle = (first + last) / 2;
-			
-			mergeSort(arr, first, middle);
-			mergeSort(arr, middle + 1, last);
-			
-			merge(arr, first, middle, last);
-		}
-	}
-
-	private static void printArray(int[] arr) {
+	private static void pirntArray(int[] arr) {
 		for (int i = 0; i < arr.length; i++)
-			System.out.print(arr[i]+" ");
-		System.out.println("");
+			System.out.print(arr[i] + " ");
 
 	}
 
-	public static void main(String[] args) {
-		int[] arr = {30, 2, 7, 3, 6, 17,1};
-		System.out.println("Given Array:");
-		printArray(arr);
-		MergeSort ms = new MergeSort();
-		ms.mergeSort(arr, 0, arr.length - 1);
-		System.out.println("Soreted Array:");
-		printArray(arr);
-	}
+	/*
+	 * private void merge(int[] arr, int first, int middle, int last) { int fn =
+	 * middle - first + 1; int ln = last - middle;
+	 * 
+	 * int[] ftemp = new int[fn]; int[] ltemp = new int[ln];
+	 * 
+	 * for (int i = 0; i < fn; ++i) ftemp[i] = arr[first + i];
+	 * 
+	 * for (int j = 0; j < ln; ++j) ltemp[j] = arr[middle + 1 + j];
+	 * 
+	 * int i = 0, j = 0; int k = first; while (i < fn && j < ln) { if (ftemp[i] <=
+	 * ltemp[j]) { arr[k] = ftemp[i]; i++; } else { arr[k] = ltemp[j]; j++; } k++; }
+	 * 
+	 * while (i < fn) { arr[k] = ftemp[i]; i++; k++;
+	 * 
+	 * }
+	 * 
+	 * while (j < ln) { arr[k] = ltemp[j]; j++; k++; }
+	 * 
+	 * }
+	 * 
+	 * void mergeSort(int[] arr, int first, int last) { if (first < last) { int
+	 * middle = (first + last) / 2;
+	 * 
+	 * mergeSort(arr, first, middle); mergeSort(arr, middle + 1, last);
+	 * 
+	 * merge(arr, first, middle, last); } }
+	 * 
+	 * private static void printArray(int[] arr) { for (int i = 0; i < arr.length;
+	 * i++) System.out.print(arr[i]+" "); System.out.println("");
+	 * 
+	 * }
+	 * 
+	 * public static void main(String[] args) { int[] arr = {30, 2, 7, 3, 6, 17,1};
+	 * System.out.println("Given Array:"); printArray(arr); MergeSort ms = new
+	 * MergeSort(); ms.mergeSort(arr, 0, arr.length - 1);
+	 * System.out.println("Soreted Array:"); printArray(arr); }
+	 */
 
 }
